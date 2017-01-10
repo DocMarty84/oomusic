7e7ffc473459125c4ba2bd74044900235de263ad
Nicolas Martinelli <nicolas.martinelli@gmail.com>
2017-01-10 01:41:47 +0100

    Improve genre browsing
    - Add genre browsing directly from the menu
    - Add a new field `artist_id` to display artists matching with a genre

--- models/oomusic_genre.py
+++ models/oomusic_genre.py
@@ -10,6 +10,7 @@ class MusicGenre(models.Model):
     name = fields.Char('Music Genre', index=True)
     track_ids = fields.One2many('oomusic.track', 'genre_id', string='Tracks')
     album_ids = fields.One2many('oomusic.album', 'genre_id', string='Albums')
+    artist_ids = fields.Many2many('oomusic.artist', string='Artists', compute='_compute_artist_ids')
     user_id = fields.Many2one(
         'res.users', string='User', index=True, required=True, ondelete='cascade',
         default=lambda self: self.env.user
@@ -18,3 +19,7 @@ class MusicGenre(models.Model):
     _sql_constraints = [
         ('oomusic_genre_name_uniq', 'unique(name, user_id)', 'Genre name must be unique!'),
     ]
+
+    def _compute_artist_ids(self):
+        for genre in self:
+            genre.artist_ids = genre.album_ids.mapped('artist_id').sorted(lambda r: r.name)
