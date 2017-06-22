73ba8c13db9ccbd095df79ed06ff3b6eb5c562a7
Nicolas Martinelli <nicolas.martinelli@gmail.com>
2017-06-23 01:13:10 +0200

    Clean-up `oomusic.suggestion`
    Remove useless code, and simplifies as much as possible.

--- models/oomusic_suggestion.py
+++ models/oomusic_suggestion.py
@@ -26,42 +26,37 @@ class MusicSuggestion(models.TransientModel):
 
     @api.depends('name_tracks')
     def _compute_track_last_played(self):
-        Track = self.env['oomusic.track'].search(
+        self.track_last_played = self.env['oomusic.track'].search(
             [('play_count', '>', 0)], order='last_play desc', limit=10)
-        self.track_last_played = Track.ids
 
     @api.depends('name_tracks')
     def _compute_track_recently_added(self):
-        Track = self.env['oomusic.track'].search([], order='create_date desc', limit=10)
-        self.track_recently_added = Track.ids
+        self.track_recently_added = self.env['oomusic.track'].search(
+            [], order='create_date desc', limit=10)
 
     @api.depends('name_tracks')
     def _compute_track_random(self):
-        params = (self.env.user.id, )
+        params = (self.env.uid, )
         query = "SELECT id FROM oomusic_track WHERE user_id = %s;"
         self.env.cr.execute(query, params)
         res = self.env.cr.fetchall()
         if not res:
             return
 
-        random_track_ids = random.sample([r[0] for r in res], min(10, len(res)))
-        Track = self.env['oomusic.track'].browse(random_track_ids)
-        self.track_random = Track.ids
+        self.track_random = random.sample([r[0] for r in res], min(10, len(res)))
 
     @api.depends('name_albums')
     def _compute_album_recently_added(self):
-        Album = self.env['oomusic.album'].search([], order='create_date desc', limit=10)
-        self.album_recently_added = Album.ids
+        self.album_recently_added = self.env['oomusic.album'].search(
+            [], order='create_date desc', limit=10)
 
     @api.depends('name_albums')
     def _compute_album_random(self):
-        params = (self.env.user.id, )
+        params = (self.env.uid, )
         query = "SELECT id FROM oomusic_album WHERE user_id = %s;"
         self.env.cr.execute(query, params)
         res = self.env.cr.fetchall()
         if not res:
             return
 
-        random_album_ids = random.sample([r[0] for r in res], min(15, len(res)))
-        Album = self.env['oomusic.album'].browse(random_album_ids)
-        self.album_random = Album.ids
+        self.album_random = random.sample([r[0] for r in res], min(15, len(res)))
