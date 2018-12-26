a765b3d2404d341858c501a283b90c8b12448541
Nicolas Martinelli <nicolas.martinelli@pm.me>
2018-12-26 01:39:35 +0100

    Delete folder
    When a non-superuser tries to delete a folder (even his own), an
    AccessError is raised on `oomusic.tracks`.
    
    A regular user doesn't have the right to delete tracks and albums. This
    is to avoid deletion from the interface. However, in this specific case,
    it should be allowed to do it.

--- models/oomusic_folder.py
+++ models/oomusic_folder.py
@@ -364,8 +364,12 @@ class MusicFolder(models.Model):
     @api.multi
     def unlink(self):
         # Remove tracks and albums included in the folders.
-        self.env['oomusic.track'].search([('folder_id', 'child_of', self.ids)]).unlink()
-        self.env['oomusic.album'].search([('folder_id', 'child_of', self.ids)]).unlink()
+        self.env['oomusic.track'].search([
+            ('folder_id', 'child_of', self.ids), ('user_id', '=', self.env.user.id)
+        ]).sudo().unlink()
+        self.env['oomusic.album'].search([
+            ('folder_id', 'child_of', self.ids), ('user_id', '=', self.env.user.id)
+        ]).sudo().unlink()
         user_ids = self.mapped('user_id')
         super(MusicFolder, self).unlink()
         for user_id in user_ids:
