ef8914b74200573ee0bd8c5052477c89a7982d12
Nicolas Martinelli <nicolas.martinelli@pm.me>
2019-01-17 22:53:23 +0100

    Specific Playlist view
    Since the behavior of the Playlist tree view is very specific, we create
    a new PlaylistView dedicated to it with the appropriate events.

--- static/src/js/view.js
+++ static/src/js/view.js
@@ -5,37 +5,42 @@ var core = require('web.core');
 var ListView = require('web.ListView');
 var ListRenderer = require('web.ListRenderer');
 var ListController = require('web.ListController');
+var viewRegistry = require('web.view_registry');
 
+
+//-----------------------------------------------------------------------------
+// ListRenderer: behavior modification
+//-----------------------------------------------------------------------------
 
 ListRenderer.include(/** @lends instance.web.ListView.List# */{
+    _renderHeaderCell: function (node) {
+        if (node.attrs.name === 'action_play' || node.attrs.name === 'action_add_to_playlist') {
+            return $('<th width=1></th>');
+        }
+        return this._super.apply(this, arguments);
+    },
+});
+
+
+//-----------------------------------------------------------------------------
+// PlaylistView
+//-----------------------------------------------------------------------------
+
+var PlaylistRenderer = ListRenderer.extend({
     init: function () {
         this._super.apply(this, arguments);
-
-        // Listen on 'oomusic_reload' only in specific playlist views
-        if (['playlist'].indexOf(this.arch.attrs.class) >= 0) {
-            core.bus.on('oomusic_reload', this, this._reloadFromPlay);
-        }
+        core.bus.on('oomusic_reload', this, this._reloadFromPlay);
     },
 
     _onRowClicked: function (event) {
         // Catch the click event on the list, so we can start playing the track
-        if (this.arch.attrs.class === 'playlist') {
-            event.preventDefault();
-            event.stopPropagation();
+        event.preventDefault();
+        event.stopPropagation();
 
-            var id = $(event.currentTarget).data('id');
-            if (id) {
-                this.trigger_up('oomusic_row_clicked_play', {id:id, target: event.target});
-            }
+        var id = $(event.currentTarget).data('id');
+        if (id) {
+            this.trigger_up('oomusic_row_clicked_play', {id:id, target: event.target});
             return;
-
-        }
-        return this._super.apply(this, arguments);
-    },
-
-    _renderHeaderCell: function (node) {
-        if (node.attrs.name === 'action_play' || node.attrs.name === 'action_add_to_playlist') {
-            return $('<th width=1></th>');
         }
         return this._super.apply(this, arguments);
     },
@@ -45,8 +50,7 @@ ListRenderer.include(/** @lends instance.web.ListView.List# */{
     },
 });
 
-
-ListController.include({
+var PlaylistController = ListController.extend({
     custom_events: _.extend({}, ListController.prototype.custom_events, {
         oomusic_row_clicked_play: '_onRowClickedPlay',
     }),
@@ -55,7 +59,15 @@ ListController.include({
         var record = this.model.get(event.data.id, {raw: true});
         core.bus.trigger('oomusic_play', record.model, record.res_id);
     },
-
 });
+
+var PlaylistView = ListView.extend({
+    config: _.extend({}, ListView.prototype.config, {
+        Controller: PlaylistController,
+        Renderer: PlaylistRenderer,
+    }),
+});
+
+viewRegistry.add('playlist_tree', PlaylistView);
 
 });
